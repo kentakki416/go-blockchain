a6668ed7ed89782d82abfa779e8ff8c2a20e9744
kentakki416 <fujiken0814@outlook.jp>
2022-05-18 10:19:43 +0900

    ブロックチェーンのストラクトを作成

--- block/blockchain.go
+++ block/blockchain.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -30,7 +31,40 @@ func (b *Block) Print() {
 	fmt.Printf("transactions   %s\n", b.transactions)
 }
 
+// ------------------------------------------------------------------------------------------
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+// ブロックチェーンの作成
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Init hash")
+	return bc
+}
+
+// ブロックチェーンの中にブロックを格納
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+// ブロッックチェーンの出力
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
 func main() {
-	b := NewBlock(0, "init hash")
-	b.Print()
+	blockChain := NewBlockchain()
+	blockChain.Print()
+	blockChain.CreateBlock(5, "hash 1")
+	blockChain.Print()
+	blockChain.CreateBlock(2, "hash 2")
+	blockChain.Print()
 }
