90f4939ec6735ca03a4bede00c1614b16702e64c
kentakki416 <fujiken0814@outlook.jp>
2022-05-23 16:09:50 +0900

    ブロックチェーンを取得するAPIの作成

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"go-blockchain/block"
+	"go-blockchain/wallet"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 )
+
+// 一度作ったブロックチェーンをcacheに格納
+var cache map[string]*block.Blockchain = make(map[string]*block.Blockchain)
 
 type BlockchainServer struct {
 	port uint16
@@ -21,13 +26,40 @@ func (bcs *BlockchainServer) Port() uint16 {
 	return bcs.port
 }
 
-// httpサーバーのハンドル
-func HelloWorld(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello World!")
+// create済みのブロックチェーンを取得
+func (bcs *BlockchainServer) GetBlockchain() *block.Blockchain {
+	// cacheからブロックチェーンを取得
+	bc, ok := cache["blockchain"]
+
+	// cahceに存在しない場合
+	if !ok {
+		// 1:Minerをブロックチェーンに登録
+		minersWallet := wallet.NewWallet()
+		bc = block.NewBlockchain(minersWallet.BlockchainAddress(), bcs.Port())
+		cache["blockchain"] = bc
+
+		// デバッグ
+		log.Printf("private_key %v", minersWallet.PrivateKeyStr())
+		log.Printf("public_key %v", minersWallet.PublicKeyStr())
+		log.Printf("blockchain_address %v", minersWallet.BlockchainAddress())
+	}
+	return bc
+}
+
+func (bcs *BlockchainServer) GetChain(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		w.Header().Add("Content-Type", "application/json")
+		bc := bcs.GetBlockchain()
+		m, _ := bc.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		log.Printf("ERROR: Invalid HTTP Method")
+	}
 }
 
 // サーバーの立ち上げ
 func (bcs *BlockchainServer) Run() {
-	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/", bcs.GetChain)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcs.Port())), nil))
 }
