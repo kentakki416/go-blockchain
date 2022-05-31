40d636406fa42b2bd6d99d742a692d5e3d33e644
kentakki416 <fujiken0814@outlook.jp>
2022-05-31 16:55:54 +0900

    ブロックチェーンの検証処理を追加

--- block/blockchain.go
+++ block/blockchain.go
@@ -45,6 +45,18 @@ func NewBlock(nonce int, previousHash [32]byte, transactions []*Transaction) *Bl
 	return b
 }
 
+func (b *Block) PreviousHash() [32]byte {
+	return b.previousHash
+}
+
+func (b *Block) Nonce() int {
+	return b.nonce
+}
+
+func (b *Block) Transactions() []*Transaction {
+	return b.transactions
+}
+
 // ブロックを出力
 func (b *Block) Print() {
 	fmt.Printf("nonce          %d\n", b.nonce)
@@ -298,6 +310,24 @@ func (bc *Blockchain) CalculateTotalAmount(blockchainAddress string) float32 {
 		}
 	}
 	return totalAmount
+}
+
+func (bc *Blockchain) ValidChain(chain []*Block) bool {
+	preBlock := chain[0]
+	currentIndex := 1
+	for currentIndex < len(chain) {
+		b := chain[currentIndex]
+		if b.previousHash != preBlock.Hash() {
+			return false
+		}
+		if !bc.ValidProof(b.Nonce(), b.PreviousHash(), b.Transactions(), MINING_DIFFICULTY) {
+			return false
+		}
+
+		preBlock = b
+		currentIndex += 1
+	}
+	return true
 }
 
 // ------------------------------------------------------------------------------------------------
